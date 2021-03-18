aa50c50cf7115d5006f47154a3b125281f62767b
timmorse13 <timmorse13@gmail.com>
2021-03-18 00:17:48 -0700

    Got homepage to show

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -2,4 +2,21 @@ const express = require('express');
 const path = require('path');
 
 const app = express(); 
-const PORT = 3000;
+const PORT = 3100;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, '../../index.html')));
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
